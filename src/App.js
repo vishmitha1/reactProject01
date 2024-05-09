9b433e923c120dd2a408feac3def8372ec7f71cc
Visal Vishmitha Alwis <106392603+vishmitha1@users.noreply.github.com>
2024-05-09 22:29:02 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import Home from "./pages/Home"
+import { BrowserRouter as Router , Routes ,Route } from "react-router-dom";
+
+import Social from "./pages/Social";
+import Store from "./pages/Store";
+import Help from "./pages/Help";
 
 function App() {
   return (
-    <div ><Home/></div>
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path='' element={<Home/>} />
+          <Route exact path='/social' element={<Social/>} />
+          <Route exact path='/help' element={<Help/>} />
+          <Route exact path='/store' element={<Store/>} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
